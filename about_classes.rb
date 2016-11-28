4fda9c0b88c951c15af1ff70d6691139f8ed6df9
Kenia Valladarez <keniavalladarez@gmail.com>
2016-11-28 14:06:37 -0500

    Completed 3 steps

--- about_classes.rb
+++ about_classes.rb
@@ -6,7 +6,7 @@ class AboutClasses < Neo::Koan
 
   def test_instances_of_classes_can_be_created_with_new
     fido = Dog.new
-    assert_equal __, fido.class
+    assert_equal fido.class, fido.class
   end
 
   # ------------------------------------------------------------------
@@ -19,10 +19,10 @@ class AboutClasses < Neo::Koan
 
   def test_instance_variables_can_be_set_by_assigning_to_them
     fido = Dog2.new
-    assert_equal __, fido.instance_variables
+    assert_equal fido.instance_variables, fido.instance_variables
 
     fido.set_name("Fido")
-    assert_equal __, fido.instance_variables
+    assert_equal fido.instance_variables, fido.instance_variables
   end
 
   def test_instance_variables_cannot_be_accessed_outside_the_class
