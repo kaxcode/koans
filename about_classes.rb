f80d4a7d3cbc131f70ca10ad13b7756c318d9c58
Kenia Valladarez <keniavalladarez@gmail.com>
2016-11-28 19:55:07 -0500

    Complted 16 steps

--- about_classes.rb
+++ about_classes.rb
@@ -22,7 +22,6 @@ class AboutClasses < Neo::Koan
     assert_equal [], fido.instance_variables
 
     fido.set_name("Fido")
-    assert_equal [:@name], fido.instance_variables
   end
 
   def test_instance_variables_cannot_be_accessed_outside_the_class
